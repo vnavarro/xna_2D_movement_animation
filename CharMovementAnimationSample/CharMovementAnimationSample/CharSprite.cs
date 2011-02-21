e075fefdc84459b1a50d35cabbaf99c39eabe21a
Vitor <vitor.navarro87@gmail.com>
2011-02-20 22:36:25 -0300

    Adding 2D animation sample

---
+++ CharMovementAnimationSample/CharMovementAnimationSample/CharSprite.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CharMovementAnimationSample
+{
+    class CharSprite : Sprite
+    {
+        private string StateLeft = "Left";
+        private string StateRight = "Right";
+        private string StateUp = "Up";
+        private string StateDown = "Down";
+        private string currentState = "Left";
+
+        private float timePerFrame;
+        private float framesPerSec = 10.0f;
+        private float totalElapsed;
+
+        private Vector2 leftDirection = new Vector2(-1, 0);
+        private Vector2 rightDirection = new Vector2(1, 0);
+        private Vector2 upDirection = new Vector2(0, -1);
+        private Vector2 downDirection = new Vector2(0, 1);
+        private Vector2 horizontalSpeed;
+        private Vector2 verticalSpeed;
+
+        Vector2 direction = Vector2.Zero;
+        Vector2 speedComposition = Vector2.Zero;
+        public ContentManager content { get; set; }
+
+        private int charHeight = 25;
+        private int charWidth = 17;
+        private int charSpeed = 160;
+
+        private Rectangle actualCharPosition;
+        private int actualCharAnimation = 0;
+        private int charX = 0;
+        private Dictionary<string, int> states = new Dictionary<string, int>();
+        private KeyboardState previousKeyboardState;
+
+
+
+        public CharSprite()
+        {
+            AssetName = "Sprites/Bomberman";
+            states.Add(StateLeft, 0);
+            states.Add(StateRight, 3);
+            states.Add(StateDown, 6);
+            states.Add(StateUp, 9);
+            timePerFrame = 1.0f / framesPerSec;
+            totalElapsed = 0;
+            horizontalSpeed = new Vector2(charSpeed, 0);
+            verticalSpeed = new Vector2(0, charSpeed);
+        }
+
+        public void LoadContent(ContentManager gameContent)
+        {
+            this.content = gameContent;
+            base.LoadContent(gameContent, AssetName);
+            Position = new Vector2(400, 150);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            int x = charX * (states[currentState] + actualCharAnimation);
+            actualCharPosition = new Rectangle(x, 0, charWidth, charHeight);
+            Draw(spriteBatch, actualCharPosition);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            totalElapsed += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (totalElapsed > timePerFrame)
+            {
+                totalElapsed -= timePerFrame;
+                KeyboardState currentKeyboardState = Keyboard.GetState();
+                UpdateMovement(currentKeyboardState);
+
+                previousKeyboardState = currentKeyboardState;
+                base.Update(gameTime, speedComposition, direction);
+            }
+        }
+
+        private void UpdateMovement(KeyboardState currentKeyboardState)
+        {
+            if (currentKeyboardState.IsKeyDown(Keys.Left) == true)
+                MoveLeft();
+            else if (currentKeyboardState.IsKeyDown(Keys.Right) == true)
+                MoveRight();
+            else if (currentKeyboardState.IsKeyDown(Keys.Up) == true)
+                MoveUp();
+            else if (currentKeyboardState.IsKeyDown(Keys.Down) == true)
+                MoveDown();
+            else
+                StayStill();
+        }
+
+        private void StayStill()
+        {
+            if (previousKeyboardState.IsKeyDown(Keys.Right))
+                currentState = StateRight;
+            else if (previousKeyboardState.IsKeyDown(Keys.Left))
+                currentState = StateLeft;
+
+            charX = charWidth;
+            actualCharAnimation = 1;
+            speedComposition = Vector2.Zero;
+            direction = Vector2.Zero;
+        }
+
+        private void MoveUp()
+        {
+            speedComposition = verticalSpeed;
+            direction = upDirection;
+
+            ChangeAnimation(StateUp);
+        }
+
+        private void MoveDown()
+        {
+            speedComposition = verticalSpeed;
+            direction = downDirection;
+
+            ChangeAnimation(StateDown);
+        }
+
+        private void MoveRight()
+        {
+            speedComposition = horizontalSpeed;
+            direction = rightDirection;
+
+            ChangeAnimation(StateRight);
+        }
+
+        private void MoveLeft()
+        {
+            speedComposition = horizontalSpeed;
+            direction = leftDirection;
+
+            ChangeAnimation(StateLeft);
+        }
+
+        private void ChangeAnimation(string state)
+        {
+            actualCharAnimation++;
+            actualCharAnimation = actualCharAnimation % 3;
+            charX = charWidth;
+            currentState = state;
+        }
+    }
+
+}
+
---
+++ CharMovementAnimationSample/CharMovementAnimationSample/Sprite.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CharMovementAnimationSample
+{
+    public class Sprite
+    {
+        public Vector2 Position = new Vector2(0, 0);
+        protected Texture2D _spriteTexture;
+        public string AssetName;
+        public Rectangle Size;
+        protected float _scale = 1.0f;
+        public float Scale
+        {
+            get { return _scale; }
+            set
+            {
+                _scale = value;
+            }
+        }
+
+        public void RescaleTexture()
+        {
+            if (_spriteTexture != null)
+                Size = new Rectangle(0, 0, (int)(_spriteTexture.Width * Scale), (int)(_spriteTexture.Height * Scale));
+        }
+
+        public void LoadContent(ContentManager gameContent, string assetName)
+        {
+            this.AssetName = assetName;
+            _spriteTexture = gameContent.Load<Texture2D>(AssetName);
+            RescaleTexture();
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            Draw(spriteBatch, new Rectangle(0, 0, _spriteTexture.Width, _spriteTexture.Height));
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch, Rectangle sourceRectangle)
+        {
+            spriteBatch.Draw(_spriteTexture, Position, sourceRectangle,
+                Color.White, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0);
+        }
+
+        public void Update(GameTime gameTime, Vector2 speed, Vector2 direction)
+        {
+            Position += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
